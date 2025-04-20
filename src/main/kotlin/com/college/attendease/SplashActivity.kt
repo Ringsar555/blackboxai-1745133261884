e260d26ad4ef13a054c870640391a43b7e1a5ca2
Ringsar555 <114125381+Ringsar555@users.noreply.github.com>
2025-04-20 12:44:55 +0530

    Add src/main/kotlin/com/college/attendease/SplashActivity.kt 1745133292364

---
+++ src/main/kotlin/com/college/attendease/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.college.attendease
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+
+    private val splashTimeout: Long = 2000 // 2 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, PortalSelectionActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashTimeout)
+    }
+}
